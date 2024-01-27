c3357cf57f52daf445410919029ce636313e8e0b
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-27 15:39:29 +0000

    added logic to render the cities from local storage on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
   var searchButton = $("#search-button");
   var cityInput = $("#search-input");
+  var ul = $("<ul>");
+
+  $("#history").append(ul);
 
   // Initialize Local Storage and Retrieve Cities from Local Storage
-  function initLS(city) {
+  function initLS() {
     var cityfromLS = localStorage.getItem("city");
     if (cityfromLS === null || cityfromLS === undefined) {
       cityfromLS = [];
@@ -22,6 +25,19 @@ $(document).ready(function () {
     cityfromLS.push(city);
     localStorage.setItem("city", JSON.stringify(cityfromLS));
   }
+
+  // Display Cities from Local Storage
+  function renderCities() {
+    cityfromLS = JSON.parse(localStorage.getItem("city")) || [];
+
+    cityfromLS.forEach((city) => {
+      var listCity = $("<li>");
+      listCity.text(city);
+      ul.append(listCity);
+    });
+  }
+
+  renderCities();
 
   // Attach the saveCity function to the Click Event
   searchButton.on("click", saveCity);

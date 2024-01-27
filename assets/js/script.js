7ef804c043c7507228604a8ee25daca46b1c23d4
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-27 14:44:25 +0000

    added logic to save cities in local storage upon button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
+  var searchButton = $("#search-button");
+  var cityInput = $("#search-input");
+
   // Initialize Local Storage and Retrieve Cities from Local Storage
   function initLS(city) {
-    var cityfromLS = localStorage.getItem(city);
+    var cityfromLS = localStorage.getItem("city");
     if (cityfromLS === null || cityfromLS === undefined) {
       cityfromLS = [];
+      localStorage.setItem("city", JSON.stringify(cityfromLS));
     } else {
       cityfromLS = JSON.parse(cityfromLS);
     }
+    return cityfromLS;
   }
+
+  // Save City upon Button Click
+  function saveCity() {
+    var city = cityInput.val();
+    cityfromLS = initLS(city);
+
+    cityfromLS.push(city);
+    localStorage.setItem("city", JSON.stringify(cityfromLS));
+  }
+
+  // Attach the saveCity function to the Click Event
+  searchButton.on("click", saveCity);
 });

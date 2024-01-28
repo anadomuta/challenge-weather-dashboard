272879012f3feb3268d23b5451690484e55db4a0
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-28 12:06:11 +0000

    added logic to display current weather for the city searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   var searchButton = $("#search-button");
   var cityInput = $("#search-input");
   var citiesButtons = $("#history");
+  var APIKey = "8fa17f3bf5819b35e5514a16ea6de259";
+  var weatherToday = $("#today");
+  //   var weatherForecast = $("#forecast");
 
   // Initialize Local Storage and Retrieve Cities from Local Storage
   function initLS() {
@@ -16,7 +19,9 @@ $(document).ready(function () {
   }
 
   // Save City upon Button Click
-  function saveCity() {
+  function saveCity(event) {
+    event.preventDefault();
+
     var city = cityInput.val();
     cityfromLS = initLS(city);
 
@@ -25,6 +30,37 @@ $(document).ready(function () {
       cityfromLS.push(city);
       localStorage.setItem("city", JSON.stringify(cityfromLS));
     }
+
+    // Building the query URL to get temperature in Celsius
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city.trim() +
+      "&units=metric" +
+      "&appid=" +
+      APIKey;
+
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        // Transfer content to HTML
+        weatherToday.html("<h3>" + cityInput.val());
+
+        var tempCelsius = $("<p>");
+        var tempInCelsius = data.main.temp;
+        tempCelsius.text("Temp: " + tempInCelsius.toFixed(2) + "°C");
+
+        var wind = $("<p>");
+        var windInKPH = data.wind.speed * 3.6;
+
+        wind.text("Wind: " + windInKPH.toFixed(2) + " KPH");
+        var humidity = $("<p>").text("Humidity: " + data.main.humidity + "%");
+
+        // Append to the HTML container
+        weatherToday.append(tempCelsius, wind, humidity);
+      });
   }
 
   // Display Cities from Local Storage
@@ -45,6 +81,9 @@ $(document).ready(function () {
   }
 
   renderCities();
+
+  // Display current weather for selected city
+  function renderCurrentWeather() {}
 
   // Attach the saveCity function to the Click Event
   searchButton.on("click", saveCity);
